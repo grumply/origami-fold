b98d097c98d4f4500efba050f2172316f61f3126
S. M. Hickman <sean@grump.ly>
2017-03-31 04:50:23 -0500

    Add a few notes to foldo.

--- src/Data/MiddleOut.hs
+++ src/Data/MiddleOut.hs
@@ -25,31 +25,54 @@ import GHC.Exts
 -- > folder (\before after total a -> total) (\_ a -> a) [undefined,False,True]
 -- ([True,True,True],True)
 class MiddleOut f where
+  -- | Dragons!
+  --
+  -- This method is oddly magical. It represents simultaneous beginning-to-end
+  -- and end-to-beginning traversal of a linearizable structure and can modify
+  -- any element in the structure as if it has already done both. It does,
+  -- of course, have limitations.
+  --
+  -- There are two rules:
+  --   1. You may choose to use one of the first two 'm's to produce the result
+  --      'm', but you must not switch during evaluation. This is equivalent to
+  --      deciding to evaluate top-down or bottom-up.
+  --   2. You may not use the last 'm' to produce the result 'm'.
+  --
+  -- Conveniently, you may use any or all of the three 'm's to produce the 'b'.
+  -- If you do so, however, be aware that using the 'b' to produce the result
+  -- 'm' will <<loop>>.
+  --
+  -- This method need only traverse the structure once and should be, in general,
+  -- linear.
+  --
+  -- The first 'm' represents the `mconcat` of the previous monoidal results.
+  -- The second 'm' represents the `mconcat` of the future monoidal results.
+  -- The third 'm' represents the final monoidal result.
   foldo :: Monoid m => (m -> m -> m) -> (m -> m -> m -> a -> (b,m)) -> f a -> (f b,m)
 
   foldsl :: Monoid m => (m -> m -> m -> a -> b) -> (m -> a -> m) -> f a -> (f b,m)
-  foldsl f g = foldo mappend $ \pm cm m a -> (f pm cm m a, g pm a)
+  foldsl f g = foldo mappend (\pm cm m a -> (f pm cm m a, g pm a))
 
   foldsl' :: Monoid m => (m -> m -> m -> a -> b) -> (m -> a -> m) -> f a -> (f b,m)
-  foldsl' f g = foldo (\x y -> let !z = mappend x y in z) $ \pm cm m a -> (f pm cm m a, g pm a)
+  foldsl' f g = foldo (\x y -> let !z = mappend x y in z) (\pm cm m a -> (f pm cm m a, g pm a))
 
   foldsr :: Monoid m => (m -> m -> m -> a -> b) -> (m -> a -> m) -> f a -> (f b,m)
-  foldsr f g = foldo (flip mappend) $ \pm cm m a -> (f pm cm m a, g pm a)
+  foldsr f g = foldo (flip mappend) (\pm cm m a -> (f pm cm m a, g pm a))
 
   foldsr' :: Monoid m => (m -> m -> m -> a -> b) -> (m -> a -> m) -> f a -> (f b,m)
-  foldsr' f g = foldo (\x y -> let !z = mappend y x in z) $ \pm cm m a -> (f pm cm m a, g pm a)
+  foldsr' f g = foldo (\x y -> let !z = mappend y x in z) (\pm cm m a -> (f pm cm m a, g pm a))
 
   foldel :: Monoid m => (m -> m -> m -> a -> b) -> (m -> a -> m) -> f a -> (f b,m)
-  foldel f g = foldo mappend $ \pm cm m a -> (f pm cm m a,g cm a)
+  foldel f g = foldo mappend (\pm cm m a -> (f pm cm m a,g cm a))
 
   foldel' :: Monoid m => (m -> m -> m -> a -> b) -> (m -> a -> m) -> f a -> (f b,m)
-  foldel' f g = foldo (\x y -> let !z = mappend x y in z) $ \pm cm m a -> (f pm cm m a,g cm a)
+  foldel' f g = foldo (\x y -> let !z = mappend x y in z) (\pm cm m a -> (f pm cm m a,g cm a))
 
   folder :: Monoid m => (m -> m -> m -> a -> b) -> (m -> a -> m) -> f a -> (f b,m)
-  folder f g = foldo (flip mappend) $ \pm cm m a -> (f pm cm m a,g cm a)
+  folder f g = foldo (flip mappend) (\pm cm m a -> (f pm cm m a,g cm a))
 
   folder' :: Monoid m => (m -> m -> m -> a -> b) -> (m -> a -> m) -> f a -> (f b,m)
-  folder' f g = foldo (\x y -> let !z = mappend y x in z) $ \pm cm m a -> (f pm cm m a,g cm a)
+  folder' f g = foldo (\x y -> let !z = mappend y x in z) (\pm cm m a -> (f pm cm m a,g cm a))
 
 instance MiddleOut [] where
   foldo c f la = (lb,final)
@@ -89,3 +112,5 @@ instance MiddleOut Seq where
                 in
                   (b <| bs,c st bs_st)
   {-# INLINE foldo #-}
+  -- TODO: implement more efficient methods for Seq that
+  --       actually deconstruct the Seq from right to left
