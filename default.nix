77676e3c8060835b4d9bdbb5eb32d292174578dc
grumply <sean@grump.ly>
2018-05-31 03:19:55 -0500

    Remove benchmark and tests to reduce dependencies in pure-platform.

--- default.nix
+++ default.nix
@@ -1,10 +1,8 @@
-{ mkDerivation, base, containers, deepseq, stdenv, trivial }:
+{ mkDerivation, base, containers, deepseq, stdenv }:
 mkDerivation {
   pname = "origami-fold";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [ base containers ];
-  testHaskellDepends = [ base containers deepseq trivial ];
-  benchmarkHaskellDepends = [ base containers deepseq trivial ];
   license = stdenv.lib.licenses.bsd3;
 }
